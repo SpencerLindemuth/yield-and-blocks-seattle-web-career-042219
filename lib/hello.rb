987f75175751501cf6cdcbf2d674ab17b8da8cd8
Spencer Lindemuth <lindemut@gmail.com>
2019-03-26 22:58:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(array)
+  if block_given?
     i = 0
 
     while i < array.length
-      yield(array[i]) {return nil}
+      yield(array[i])
       i = i + 1
     end
+  else
+    puts "No block given!"
+  end
 
     array
 end
